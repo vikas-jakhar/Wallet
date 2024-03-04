dc406649d4784eadc72bb92cc083bafa285d102b
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-03-04 16:24:24 +0530

    first commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,21 @@
+const searchicon = document.querySelector(".search-icon");
+const searchbox = document.querySelector(".search-box");
+
+searchicon.addEventListener("click", function () {
+    searchbox.classList.toggle("show1")
+})
+
+const menuicon = document.querySelector(".menu-icon");
+const menulist = document.querySelector(".menu-list");
+
+menuicon.addEventListener("click", function () {
+    menulist.classList.toggle("show1")
+})
+
+const heart = document.querySelectorAll(".heart");
+
+heart.forEach(function (e) {
+    e.addEventListener("click", function () {
+        e.classList.toggle("red")
+    });
+})
