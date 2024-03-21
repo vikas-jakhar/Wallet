9d3b04a78a5fc83dbbdd7f6073dc89cddc4c9bfa
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-03-21 15:02:05 +0530

    update image

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,14 +3,14 @@ const searchbox = document.querySelector(".search-box");
 
 searchicon.addEventListener("click", function () {
     searchbox.classList.toggle("show1")
-})
+});
 
 const menuicon = document.querySelector(".menu-icon");
 const menulist = document.querySelector(".menu-list");
 
 menuicon.addEventListener("click", function () {
     menulist.classList.toggle("show1")
-})
+});
 
 const heart = document.querySelectorAll(".heart");
 
@@ -18,4 +18,4 @@ heart.forEach(function (e) {
     e.addEventListener("click", function () {
         e.classList.toggle("red")
     });
-})
+});
